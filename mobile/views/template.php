d8e14168a2149cccbcdfcc19597e8843bf7a5fb8
Jeremy <31535467@QQ.COM>
2017-03-08 22:54:47 +0800

    完善招聘列表功能 完善招聘详情功能 完善申请职位功能(未完)

--- mobile/views/template.php
+++ mobile/views/template.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html class="no-js" lang="zh-CN" dir="ltr">                        
 <head>
-    <title><?php echo $title ? $title: '';?></title>
+    <title></title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1" />
     <meta http-equiv="X-UA-Compatible" content="IE=9" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <!-- 通用 -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
